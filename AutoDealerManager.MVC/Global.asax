3fb11af80b134ddd49a0ce9fbeb2b822e3a63ff3
VitorSilva <vitorviniciusdev@gmail.com>
2024-11-01 15:54:16 -0300

    add_folder_structure

---
+++ AutoDealerManager.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoDealerManager.MVC.MvcApplication" Language="C#" %>
---
+++ AutoDealerManager.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoDealerManager.MVC.Startup))]
+namespace AutoDealerManager.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
