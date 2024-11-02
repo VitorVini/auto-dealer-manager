d324403008fa28f844f321ac36736bfe39306835
VitorSilva <vitorviniciusdev@gmail.com>
2024-11-02 20:07:48 -0300

    remove_mvc_identity

--- AutoDealerManager.MVC/Properties/AssemblyInfo.cs
+++ AutoDealerManager.MVC/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f733d88a-b5e4-4e92-8406-a187b863c17c")]
+[assembly: Guid("de39f4d9-6c13-4e54-a5f2-65abf7aa2381")]
 
 // Version information for an assembly consists of the following four values:
 //
